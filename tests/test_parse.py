2223bed436c4a23784426531bc2f489d9d6c2452
Isaac Johnson <ijohnson@wikimedia.org>
2022-06-23 18:11:52 +0000

    Create initial structure for tests with headings as example.

---
+++ tests/test_parse.py
@@ -0,0 +1,11 @@
+from context import Article, example_html_one, example_html_two
+
+def test_get_headings_one():
+    article = Article(example_html_one)
+    expected_headers = []
+    assert article.get_headers() == expected_headers
+
+def test_get_headings_two():
+    article = Article(example_html_two)
+    expected_headers = ['Life', 'References']
+    assert article.get_headers() == expected_headers
