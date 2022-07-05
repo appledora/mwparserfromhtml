4ea7d5b483f7f52bb29888b3891cda9ef8be13fc
Appledora <nimzia.blu@gmail.com>
2022-07-05 11:39:37 +0000

    Update: cleaned utils.py

--- src/parse/utils.py
+++ src/parse/utils.py
@@ -2,24 +2,6 @@ import re
 
 from bs4 import Comment  # for parsing the HTML
 
-# regex for checking if a string is a valid URL
-regex = re.compile(
-    r'^(?:http|ftp)s?://'  # http:// or https://
-    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
-    r'localhost|'  # localhost...
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
-    r'(?::\d+)?'  # optional port
-    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-
-htmlCodes = [
-    ["&", "&amp;"],
-    ["<", "&lt;"],
-    [">", "&gt;"],
-    ['"', "&quot;"],
-]
-## actually can't remember why I did this - but it seems to work
-htmlCodesReversed = htmlCodes[:]
-htmlCodesReversed.reverse()
 
 def is_comment(element):
     return isinstance(element, Comment)
@@ -44,22 +26,6 @@ def nested_value_extract(key, var):
                             yield result
 
 
-def tag_visible(element):
-    if element.parent.name in [
-        "style",
-        "script",
-        "head",
-        "title",
-        "meta",
-        "[document]",
-    ]:
-        return False
-    if isinstance(element, Comment):
-        return False
-    if re.match(r"\[\d+\]", str(element)):
-        return False
-    return True
-
 
 def flatten_list(A):
     rt = []
@@ -70,16 +36,6 @@ def flatten_list(A):
             if i.strip() != "":
                 rt.append(i)
     return rt
-
-
-
-
-def htmlDecode(s, codes=htmlCodesReversed):
-    """Returns the ASCII decoded version of the given HTML string. This does
-    NOT remove normal HTML tags like <p>. It is the inverse of htmlEncode()."""
-    for code in codes:
-        s = s.replace(code[1], code[0])
-    return s
 
 
 
