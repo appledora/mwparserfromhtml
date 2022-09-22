cede9352c43013ab70d511e780eb8fd81bfaa183
appledora <appledora@pop-os.localdomain>
2022-09-23 02:18:59 +0600

    update: README.md example updates + added two basic tutorial notebooks

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 DESCRIPTION = 'Wikipedia HTML Dump Parsing'
 LONG_DESCRIPTION = 'A package that supports plaintext and object extraction from Wikipedia HTML dumps.'
 
